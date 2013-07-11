b831a1467db56077c0301a14eede368908489fe6
Nick Peachey <nick.peachey@gmail.com>
2013-07-11 16:06:20 +0100

    added rest of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aofaces::Application.config.secret_token = '525272ccdc2e264db81c6fcbf4038543873faa8002fa4a4c17b7369ef1dca6085b242d7a88f0e70cf5f04dc334a4194bfa56eaf793ea1de895497b0eeb9f528e'
